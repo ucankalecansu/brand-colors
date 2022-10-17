fa47492123d5368732a089c871f284dc5cae9651
Cansu Uçankale <54913401+ucankalecansu@users.noreply.github.com>
2022-10-18 00:33:25 +0300

    indirme işlemi, dosya tipi seçimi, prompt eklendi

--- src/components/Content.js
+++ src/components/Content.js
@@ -4,15 +4,17 @@ import Brand from './Brand';
 import { useContext } from 'react';
 import MainContext from '../MainContext';
 import LazyLoad from 'react-lazyload';
+import Download from './Download';
 
 export default function Content() {
 
-    const {brands}= useContext(MainContext)
+    const {brands,selectedBrands}= useContext(MainContext)
 
   return (
     <main className='content'>
         <header className='header'>
             <Search/>
+            {selectedBrands.length !==0 && <Download/>}
         </header>
         <section className='brands'>
             {brands.map((brand)=>{
